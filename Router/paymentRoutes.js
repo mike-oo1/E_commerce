f22f54ca0bd5a7a193474a6c8d2c6340fb25892b
mikeoo1 <micthomas407@gmail.com>
2023-11-09 13:09:05 +0100

    LatestPush

--- Router/paymentRoutes.js
+++ Router/paymentRoutes.js
@@ -1,6 +1,5 @@
 const express =require("express")
 const Router =express()
-
 const uploads = require("../Utils/identityCardMulter")
 const upload =require("../Utils/giftCardImageMulter")
 const cloudinary =require("../Utils/cloudinary")
@@ -8,8 +7,6 @@ const {debitCardDetails}= require("../Controllers/debitCardPayment")
 const {cardPayments,CardPayment}= require("../Controllers/giftCardpaymentController")
 
 Router.route("/debitcardPayment").post(debitCardDetails)
-// Router.route("/price/:id").get(payWithGiftCard)
-// Router.route("/giftCard/:id").post(uploads,cardPayments)
 Router.route("/giftCard/:id").post(uploads.single("IdentityCardFront"),cardPayments)
 
 Router.route("/payment").post(upload.single("GiftCardImage"),CardPayment)
